d5a3518a7c5ef77c57beef674f424c9c4d80775c
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-03-01 21:19:20 +0530

    partials and create helper function

--- helpers.php
+++ helpers.php
@@ -8,3 +8,21 @@
 function basePath($path = '') {
    return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view 
+ * @param string $name
+ * @return void
+ */
+function loadView($name) {
+   require basePath("views/{$name}.view.php");
+}
+
+/**
+ * Load a partials
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name) {
+   require basePath("views/partials/{$name}.php");
+}
