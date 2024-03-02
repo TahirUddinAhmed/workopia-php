0b132408177be5760d0a28d1a6167ff59295895c
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-03-02 21:08:32 +0530

    create simple routes

--- public/index.php
+++ public/index.php
@@ -2,4 +2,20 @@
 require '../helpers.php';
 
 // require basePath('views/home.view.php');
-loadView('home');
+// loadView('home');
+$routes = [
+  '/' => 'controllers/home.php',
+  '/listings' => 'controllers/listings/index.php',
+  '/listings/create' => 'controllers/listings/create.php',
+  '404' => 'controllers/error/404.php'
+];
+
+// Router logic
+$uri = $_SERVER['REQUEST_URI'];
+
+// inspactAndDie($uri);
+if(array_key_exists($uri, $routes)) {
+  require basePath($routes[$uri]);
+} else {
+  require basePath($routes['404']);
+}
