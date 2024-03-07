ce3961e9747968865edc1d452bb65ef45a7612f5
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-03-07 18:25:08 +0530

    single listing fetch data and display

--- Database.php
+++ Database.php
@@ -28,13 +28,20 @@ class Database {
     /**
      * Query the database
      * 
-     * @param string @query
+     * @param string $query
+     * @param array $params
      * @return PDOStatement
      * @throws PDOException
      */
-    public function query($query) {
+    public function query($query, $params = []) {
         try {
             $stmt = $this->conn->prepare($query);
+            
+            // Bind named params
+            foreach($params as $param => $value) {
+                $stmt->bindValue(':' . $param, $value);
+            }
+
             $stmt->execute();
 
             return $stmt;
