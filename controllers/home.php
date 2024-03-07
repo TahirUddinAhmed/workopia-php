552eba331fbc2d759ca6356a22b41f5393a9991a
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-03-07 11:05:14 +0530

    Display data on the home view

--- controllers/home.php
+++ controllers/home.php
@@ -1,10 +1,13 @@
 <?php
 
 $config = require basePath('config/db.php');
+// Instiating Database class
 $db = new Database($config);
 
 $listings = $db->query("SELECT * FROM listings LIMIT 6")->fetchAll();
 
-inspact($listings);
+// inspact($listings);
 
-loadView('home');
+loadView('home', [
+    'listings' => $listings,
+]);
